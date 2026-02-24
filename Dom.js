6583db521237f1089e1d8527d06b5831eb3c7fa3
M S Ganesh <ganeshsmydur@gmail.com>
2026-02-24 17:25:24 +0530

    Add Dom

--- Dom.js
+++ Dom.js
@@ -1,11 +1,23 @@
-let div =document.querySelector("div");
-console.log(div);
+// let div =document.querySelector("div");
+// console.log(div);
 
-let id = div.getAttribute("id");
-console.log(id);
+// let id = div.getAttribute("id");
+// console.log(id);
 
-let name = div.getAttribute("name");
-console.log(name);
+// let name = div.getAttribute("name");
+// console.log(name);
 
-let para = document.querySelector(".para");
-console.log(para.getAttribute("class"));
+// let para = document.querySelector(".para");
+// console.log(para.getAttribute("class"));
+// // getAttribute() method is used to retrieve the value of a specified attribute from an HTML element. It takes the name of the attribute as an argument and returns its value as a string. If the attribute does not exist, it returns null.
+
+// let para1 = document.querySelector(".para");
+// console.log(para1.setAttribute("class" ,"new-class"));
+
+let div = document.querySelector("div");
+div.style.backgroundColor = "blue";
+div.style.backgroundColor = "green";
+div.style.fontSize = "20px";
+div.innerText = "Hello!";
+
+
